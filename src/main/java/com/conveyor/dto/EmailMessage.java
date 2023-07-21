2f377338d4f0a406f8ae2b9b77c6078ca55864a8
Sermjazhko <seldead@mail.ru>
2023-07-21 23:15:51 +0300

    create

---
+++ src/main/java/com/conveyor/dto/EmailMessage.java
@@ -0,0 +1,17 @@
+package com.conveyor.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class EmailMessage {
+
+    private String address;
+    private Enum theme;
+    private Long applicationId;
+}
